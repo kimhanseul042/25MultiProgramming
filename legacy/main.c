36d6042abdd76b4ec3f712ffef67e0e033d1d122
kimhanseul <khsksh06@gwnu.ac.kr>
2025-04-09 12:51:21 +0900

    hello

---
+++ legacy/main.c
@@ -0,0 +1,23 @@
+int titel_screen()
+{
+	puts("*******************");
+	puts("love.....love......");
+	puts("*******************");
+	
+	puts("");
+	return 0;
+}
+
+int main()
+{
+	titel_screen();
+	titel_screen();
+	titel_screen();
+	titel_screen();
+	titel_screen();
+
+	puts("hello");
+	return 0;
+}
+
+
